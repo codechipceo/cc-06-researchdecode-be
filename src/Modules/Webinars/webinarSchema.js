ab5dfed2a66785a4d83113dc6c7be5a84f4197b8
anish2210 <anishjaiswal1220@gmail.com>
2024-12-05 12:55:59 +0530

    Webinar Schema created

--- src/Modules/Webinars/webinarSchema.js
+++ src/Modules/Webinars/webinarSchema.js
@@ -1,9 +1,67 @@
-const mongoose=require("mongoose")
+const mongoose = require("mongoose");
 
-const webinarSchema=new mongoose.Schema({
+const webinarSchema = new mongoose.Schema({
+  webinarCreator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "profile",
+    require: true
+  },
+  webinarTitle: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  webinarDate: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  webinarTime: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  webinarSpeaker: {
+    type: String,
+    trim: true,
+  },
+  webinarSponsor: {
+    type: String,
+    trim: true,
+  },
+  webinarLocation: {
+    type: String,
+    trim: true,
+  },
+  webinarMode: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  webinarDescription: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+});
 
+const webinarRegistration = new mongoose.Schema({
+  webinarId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref:"Webinar",
+    require: true,
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Student",
+    require: true,
+  },
+});
 
+const WebinarModel = new mongoose.model("Webinar", webinarSchema);
+const WebinarRegistrationModel = new mongoose.model(
+  "WebinarRegistration",
+  webinarRegistration
+);
 
-})
-const WEBINAR=new mongoose.model("webinar",webinarSchema)
-module.exports=WEBINAR;
+module.exports = { WebinarModel, WebinarRegistrationModel };
